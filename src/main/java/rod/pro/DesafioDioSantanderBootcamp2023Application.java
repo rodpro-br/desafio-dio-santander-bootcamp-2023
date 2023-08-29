f64a261ae515a21f78d44bae624c0858fc3cd6ad
Rodrigo Pronunciati <rodpro@hotmail.com>
2023-08-29 13:46:53 -0300

    Initial commit

---
+++ src/main/java/rod/pro/DesafioDioSantanderBootcamp2023Application.java
@@ -0,0 +1,13 @@
+package rod.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioSantanderBootcamp2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioSantanderBootcamp2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/rod/pro/DesafioDioSantanderBootcamp2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package rod.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioSantanderBootcamp2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
