50e0aa248c0e383c6b547085fd9e60b9be9aec78
Rodrigo Pronunciati <rodpro@hotmail.com>
2023-09-04 14:16:35 -0300

    Bugfix CORS

--- src/main/java/rod/pro/DesafioDioSantanderBootcamp2023Application.java
+++ src/main/java/rod/pro/DesafioDioSantanderBootcamp2023Application.java
@@ -1,8 +1,11 @@
 package rod.pro;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url="/", description= "Default server url")} )
 @SpringBootApplication
 public class DesafioDioSantanderBootcamp2023Application {
 
